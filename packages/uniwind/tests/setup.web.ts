633a67d392836d03f79dbcc67879a42c5289c5e0
Hubert Bieszczad <48803618+Brentlok@users.noreply.github.com>
2026-02-12 09:52:30 +0100

    fix: withUniwind merge with inline (#378)
    * fix: withUniwind merge with inline
    
    * test: setup web tests
    
    * test: more cases for withUniwind-native
    
    * test: basic withUniwind test for web
    
    * test: withUniwind web complete tests
    
    * chore: minor changes

---
+++ packages/uniwind/tests/setup.web.ts
@@ -0,0 +1,30 @@
+// Mock CSS-related globals that JSDOM doesn't define
+if (typeof CSSRuleList === 'undefined') {
+    global.CSSRuleList = class CSSRuleList {} as any
+}
+
+if (typeof CSSRule === 'undefined') {
+    global.CSSRule = class CSSRule {
+        parentRule: CSSRule | null = null
+    } as any
+}
+
+if (typeof CSSStyleRule === 'undefined') {
+    global.CSSStyleRule = class CSSStyleRule extends global.CSSRule {} as any
+}
+
+if (typeof CSSMediaRule === 'undefined') {
+    global.CSSMediaRule = class CSSMediaRule extends global.CSSRule {
+        media: MediaQueryList | null = null
+    } as any
+}
+
+// Mock Uniwind for web tests
+// Since we're just testing that className props are passed through,
+// we don't need full Uniwind initialization
+beforeAll(() => {
+    // Set up document class for theme support
+    if (typeof document !== 'undefined') {
+        document.documentElement.className = 'light'
+    }
+})
